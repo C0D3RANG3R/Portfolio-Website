5e93b00bd23138ecf9c25f79496b222019bf33e1
Sagnik <sagnik.papun02@gmail.com>
2022-02-22 20:53:59 +0530

    Initial commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,71 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Lorem Ipsum",
+        tags:["react"],
+        date:"20 Feb, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:"#"
+    },
+    {
+        id:2,
+        name:"Lorem Ipsum",
+        tags:["react"],
+        date:"20 Feb, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:"#"
+    },
+    {
+        id:3,
+        name:"Lorem Ipsum",
+        tags:["react"],
+        date:"20 Feb, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:"#"
+    },
+    {
+        id:4,
+        name:"Lorem Ipsum",
+        tags:["react"],
+        date:"20 Feb, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:"#"
+    },
+    {
+        id:5,
+        name:"Lorem Ipsum",
+        tags:["react"],
+        date:"20 Feb, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:"#"
+    },
+    {
+        id:6,
+        name:"Lorem Ipsum",
+        tags:["react"],
+        date:"20 Feb, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:"#"
+
+    },
+   
+    {
+        id:7,
+        name:"Lorem Ipsum",
+        tags:["react"],
+        date:"20 Feb, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:"#"
+    },
+    {
+        id:8,
+        name:"Lorem Ipsum",
+        tags:["react"],
+        date:"20 Feb, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:"#"
+    },
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,62 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Lorem Ipsum",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        tags:["react"],
+        
+        demo:"#",
+        github:"#"
+    },
+    {
+        id:2,
+        name:"Lorem Ipsum",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        tags:["react"],
+        
+        demo:"#",
+        github:"#"
+        
+    },
+    {
+        id:3,
+        name:"Lorem Ipsum",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        tags:["react"],
+        
+        demo:"#",
+        github:"#"
+    },
+    {
+        id:4,
+        name:"Lorem Ipsum",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        tags:["react"],
+        
+        demo:"#",
+        github:"#"
+    },{
+        id:5,
+        name:"Lorem Ipsum",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        tags:["react"],
+        
+        demo:"#",
+        github:"#"
+        
+    },
+    {
+        id:6,
+        name:"Lorem Ipsum",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        tags:["react"],
+        
+        demo:"#",
+        github:"#"
+        
+    },
+    
+  
+] 
